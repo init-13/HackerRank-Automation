01c9363de92d198b3863ccd011b8e7efa42525f4
Nitin Das <nitinhash1308@gmail.com>
2022-04-04 19:22:34 +0530

    :fire: added Details and waitClick function

---
+++ detail.js
@@ -0,0 +1,17 @@
+const details = {
+	browser: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
+	siteLink: "http://www.hackerrank.com/auth/login",
+	username: "testmane@rhyta.com",//You can use yours credential here
+	password: "testmane",
+	btnLogin: ".ui-btn.ui-btn-large,ui-btn-primary.auth-button.ui-btn-styled",
+	btnAlgorithm: "div[data-automation='algorithms']",
+	btnAllQ: 'a[data-analytics="ChallengeListChallengeName"]'
+	selCE: ".checkbox-input",
+	selCustom: ".custominput",
+	selEditor: ".monaco-editor.no-user-select.vs",
+	btnsubmit: ".hr-monaco-submit"
+};
+
+module.export = details;
+
+// buttons to press -> Login -> Algorithms -> AllQuestion -> Submit
---
+++ functions.js
@@ -0,0 +1,31 @@
+function waitAndClick(btnSelector,tabObj){
+
+	const waitAndClickPromise = new Promise(function(resolve,reject){
+
+		let waitForSelector = tabObj.waitForSelector(btnSelector);
+
+		waitForSelector
+		.then(function(){
+
+			let clickbtn = tabObj.click(btnSelector);
+			return clickbtn;
+		})
+		.then(function(){
+
+			resolve();
+		})
+		.catch(function(err){
+			console.log(err+"while Wait and click")
+			reject();
+		})
+	});
+
+	return waitAndClickPromise;
+
+}
+
+
+function solveSubmitProblem(link,tabObj){
+
+	
+}
